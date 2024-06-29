56a78c5aaa851ebffc9dd3e40b1111ad702134f5
jaquesaurelio2 <jaquesaurelio1@gmail.com>
2024-06-29 17:19:03 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import styles from "./app.module.css";
+import data from "./data";
+
+export default function App() {
+  return <div className={styles.container}></div>;
+}
